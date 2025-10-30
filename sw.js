64809b489ee24961b10a733afd6a72853a75ea42
weolopez@gmail.com <weolopez@gmail.com>
2025-10-30 09:25:57 -0400

    Add service worker for caching and offline support; include manifest and icons

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'we-os-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/desktop/src/components/desktop-component.js',
+  '/desktop/desktop.json'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  console.log('Service worker installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activating...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
