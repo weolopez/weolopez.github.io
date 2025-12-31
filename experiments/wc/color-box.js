5b233fd53a60086837983591db9065ef25e21486
weolopez <octocat@github.com>
2025-12-31 11:51:11 -0500

    Sync experiments/wc/color-box.js

--- experiments/wc/color-box.js
+++ experiments/wc/color-box.js
@@ -1,4 +1,4 @@
-export class ColorBox extends HTMLElement {
+class ColorBox extends HTMLElement {
   static observedAttributes = ['color'];
   attributeChangedCallback(_, __, v) { this.color = v; this.render(); }
   connectedCallback() { this.render(); }
@@ -6,7 +6,5 @@ export class ColorBox extends HTMLElement {
     this.innerHTML = `<div style="border:1px solid var(--border);border-radius:12px;padding:16px;"><h3>Color Box</h3><div style="height:80px;border-radius:8px;background:${this.color}"></div></div>`;
   }
 }
+customElements.define('color-box', ColorBox);
 
-if (!customElements.get('color-box')) {
-  customElements.define('color-box', ColorBox);
-}
