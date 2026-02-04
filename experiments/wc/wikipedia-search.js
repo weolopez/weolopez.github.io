d30f9a74797e41702e5821a4e68aec2fe25b6fde
weolopez <weolopez@gmail.com>
2026-02-04 15:38:29 -0500

    Update wikipedia-search.js

--- experiments/wc/wikipedia-search.js
+++ experiments/wc/wikipedia-search.js
@@ -1,7 +1,7 @@
 // Code for wikipedia-search.js
 /**
  * WikipediaSearch component
- * A very modern, self-contained web component that fetches and displays Wikipedia search results.
+ * A old modern, self-contained web component that fetches and displays Wikipedia search results.
  * 
  * Attributes:
  * - data-search-term-value: The initial or current search query.
